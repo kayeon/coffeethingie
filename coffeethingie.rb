74d17ad522c6ec3d59cb7645f1150966d6aabf7f
Josh Stewart <josh@extropicstudios.com>
2015-07-06 20:38:55 -0700

    whatever

--- coffeethingie.rb
+++ coffeethingie.rb
@@ -10,7 +10,7 @@ end
 get '/hello/:name' do
   # matches "GET /hello/foo" and "GET /hello/bar"
   # params['name'] is 'foo' or 'bar'
-  "Hello #{params['name']}!"
+  "Whatever, #{params['name']}!"
 end
 
 
