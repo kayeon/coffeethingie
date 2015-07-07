f1e5e64ba2ff54c7f2c4f58e4acc7eb49cf0ea58
kayeon <kayeon@gmail.com>
2015-07-06 21:35:44 -0700

    add erb templates

--- coffeethingie.rb
+++ coffeethingie.rb
@@ -4,10 +4,24 @@ require 'sinatra'
 # /hello/test
 
 get '/' do
-  'Hello world!'
+  # erb(:index);
+
+  erb :index, { :locals => { :page_header => "Coffee Shop Thingie" }}
 end
 
+
+get '/images' do
+ # http://www.sinatrarb.com/intro.html#Sending%20Files
+  send_file 'poe.jpg'
+end
+
+
 get '/hello/:name' do
+  # :name is the key to map the value from the route into the params object
+  # Example
+  # -------
+  # /hello/poe will map "poe" into params[:name]
+
   # matches "GET /hello/foo" and "GET /hello/bar"
   # params['name'] is 'foo' or 'bar'
   "bonjour #{params['name']}! Whatever, #{params['name']}!"
